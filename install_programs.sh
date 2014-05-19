6d50e5482bb9a8ac105253d3663e4c386bdf92ef
Viktor Smari <viktorsmari@gmail.com>
2014-05-19 15:37:29 +0000

    Removed javaws

--- install_programs.sh
+++ install_programs.sh
@@ -1,1 +1,1 @@
-sudo apt-get install parcellite javaws ipcalc curl nmap vim zsh git git-core arandr g++ make chromium-browser tree scrot bc
+sudo apt-get install parcellite ipcalc curl nmap vim zsh git git-core arandr g++ make chromium-browser tree scrot bc
