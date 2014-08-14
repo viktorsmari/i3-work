49ff7f65b240fa212faf5990514957255bd6ae73
Viktor Smari <viktora@advania.is>
2014-08-14 09:11:30 +0000

    Renaming and adding thunar with icon support

--- viktor.zsh
+++ viktor.zsh
@@ -3,11 +3,13 @@
 #ruby
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm" 
 
+# ruby env
+export PATH="$HOME/.rbenv/bin:$PATH"
+
 alias weather=" nohup /opt/extras.ubuntu.com/my-weather-indicator/bin/my-weather-indicator &"
 alias open=xdg-open
 alias xclip='xclip -selection c'
 alias edit='vi ~/.i3/viktor.zsh'
-alias fresh='vi ~/.i3/freshInstall.txt'
 alias pi='ping mbl.is'
 alias fugl='nohup ~/.i3/pidginscript.sh &'
 alias volup='pactl set-sink-volume 1 +10%'
@@ -26,6 +28,4 @@ alias AMS="timedatectl set-timezone Europe/Amsterdam"
 alias KOB="timedatectl set-timezone Europe/Copenhagen"
 alias LON="timedatectl set-timezone Europe/London"
 
-# ruby env
-export PATH="$HOME/.rbenv/bin:$PATH"
 
