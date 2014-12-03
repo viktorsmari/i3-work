d4092b544e1291b4e22c11433381672fb7b4ca3e
Viktor Smari <viktorsmari@gmail.com>
2014-12-03 11:06:17 +0100

    Vim can save with ctrl-s.
    Pinging google instead of mbl

--- viktor.zsh
+++ viktor.zsh
@@ -1,21 +1,25 @@
 # This file is public on github
 
-#ruby
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm" 
-
-# ruby env
-export PATH="$HOME/.rbenv/bin:$PATH"
-
+#config
+#xset r rate 250
+alias conf="cat ~/.i3/viktor.zsh | grep alias"
+alias edit='vi ~/.i3/viktor.zsh'
 alias prog='echo gpick, xev, xmodmap -pke'
 alias getprocid='xprop | grep WM_CLASS'
+stty stop undef
+
+#handy
 alias www='cd /var/www/html/'
 alias weather=" nohup /opt/extras.ubuntu.com/my-weather-indicator/bin/my-weather-indicator &"
 alias open=xdg-open
 alias xclip='xclip -selection c'
-alias edit='vi ~/.i3/viktor.zsh'
-alias pi='ping mbl.is'
+alias pi='ping 8.8.8.8'
+
+#vol
 alias volup='pactl set-sink-volume 1 +10%'
 alias vold='pactl set-sink-volume 1 10%'
+
+#adv
 alias avahifix='sudo service avahi-daemon stop'
 alias tunnel='echo ssh -L 8080:localhost:8443 user@host'
 
@@ -25,10 +29,7 @@ alias la='ls -lAhp --group-directories-first'
 
 # Switch timezones
 alias IS="timedatectl  set-timezone Atlantic/Reykjavik"
-alias LA="timedatectl set-timezone America/Los_Angeles"
-alias NY="timedatectl set-timezone America/New_York"
 alias AMS="timedatectl set-timezone Europe/Amsterdam"
 alias KOB="timedatectl set-timezone Europe/Copenhagen"
-alias LON="timedatectl set-timezone Europe/London"
 
 
