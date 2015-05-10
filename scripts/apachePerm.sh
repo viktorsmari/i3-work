029eadce89728a32d88c01aadd43a98772270d25
Viktor Smari <viktorsmari@gmail.com>
2015-05-10 17:16:12 +0200

    Update scripts

--- scripts/apachePerm.sh
+++ scripts/apachePerm.sh
@@ -1,4 +1,5 @@
-# Make /var/www editable by user. Takes user as parameter
-sudo adduser $! www-data
+# Make /var/www editable by user. 
+sudo adduser $(whoami)  www-data
 sudo chown -R www-data:www-data /var/www
-sudo chmod -R g+rw /var/www
+sudo chmod -R g+rwX /var/www
+echo "You need to re-log in if for changes to take effect"
