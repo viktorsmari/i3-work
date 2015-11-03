366549072e2257a264b32c17163c83b018e892db
Viktor Smari <viktorsmari@gmail.com>
2015-11-03 09:20:38 +0100

    mod i|o for Icelandic, english with notify msg

---
+++ kb-en.sh
@@ -0,0 +1,3 @@
+/usr/bin/setxkbmap -option ""
+/usr/bin/setxkbmap -option "caps:escape, grp:switch, grp:alt_shift_toggle" -layout is,us
+notify-send 'English' -t 1000 
--- kb-is.sh
+++ kb-is.sh
@@ -1,2 +1,3 @@
 /usr/bin/setxkbmap -option ""
 /usr/bin/setxkbmap -option "caps:escape, grp:switch, grp:alt_shift_toggle" -layout us,is
+notify-send 'Icelandic' -t 1000 
