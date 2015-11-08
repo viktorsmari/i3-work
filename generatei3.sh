646edb8d5bf927dec4c3330c85f178009aa517de
Viktor Smari <viktorsmari@gmail.com>
2015-11-08 22:38:07 +0100

    Rename file

--- generatei3.sh
+++ generatei3.sh
@@ -3,7 +3,7 @@ HOST=$(hostname)
 
 if [ "$HOST" = studio ]; then
     printf " using leno i3"
-	cat ~/.i3/config.base ~/.i3/config-lenovo > ~/.i3/config
+	cat ~/.i3/config.base ~/.i3/config-lenovo-home > ~/.i3/config
 else
     printf " using ASUS i3"
 	cat ~/.i3/config.base ~/.i3/config-work > ~/.i3/config
