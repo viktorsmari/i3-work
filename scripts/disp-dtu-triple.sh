1b022b4711e98a9f9fad19e4465236ddd2d9e351
Viktor Smari <viktorsmari@gmail.com>
2015-11-27 08:53:27 +0100

    DTU 3 mon setup

---
+++ scripts/disp-dtu-triple.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VIRTUAL1 --off --output eDP1 --primary --mode 1920x1080 --pos 3610x0 --rotate normal --output DP1 --mode 2560x1440 --pos 1050x0 --rotate normal --output DP2-1 --mode 1680x1050 --pos 0x0 --rotate left --output DP2-2 --off --output DP2-3 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --off
