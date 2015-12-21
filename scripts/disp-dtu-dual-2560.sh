1265ee078bcba13f47e697aca9836439aec6953a
Viktor Smari <viktorsmari@gmail.com>
2015-12-21 08:43:28 +0100

    Cleanup, remove reduntant scripts

--- scripts/disp-dtu-dual-2560.sh
+++ scripts/disp-dtu-dual-2560.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output VIRTUAL1 --off --output eDP1 --off --output DP1 --primary --mode 2560x1440 --pos 2560x0 --rotate normal --output DP2-1 --off --output DP2-2 --off --output DP2-3 --off --output HDMI2 --mode 2560x1440 --pos 0x0 --rotate normal --output HDMI1 --off --output DP2 --off
+xrandr --output VIRTUAL1 --off --output eDP1 --off --output DP1 --primary --mode 2560x1440 --pos 1440x0 --rotate normal --output HDMI2 --mode 2560x1440 --pos 0x0 --rotate left --output HDMI1 --off --output DP2 --off
