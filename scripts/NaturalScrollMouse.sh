c6dfdfc7bfe7c3a56939351665d56ba386b69a0a
Viktor Smari <viktorsmari@gmail.com>
2016-02-26 14:03:13 +0100

    Script for inverse scrolling

---
+++ scripts/NaturalScrollMouse.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+MOUSEID=$(xinput list |grep Anywhere|cut -c 55-56)
+echo $MOUSEID
+
+DEVICE=$(xinput list-props $MOUSEID |grep Scroll | cut -c 28-30)
+echo $DEVICE
+
+# EXAMPLE: Evdev Scrolling Distance (378):	1, 1, 1
+
+echo `xinput set-prop $MOUSEID $DEVICE -1, -1, -1`
+#reversing 1 to -1
