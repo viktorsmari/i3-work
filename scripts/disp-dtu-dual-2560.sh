943fed8fab4b5a143fe8dddbbe347c1fbdecf56a
viktorsmari <viktorsmari@gmail.com>
2016-08-19 13:24:30 +0200

    Switch monitors

--- scripts/disp-dtu-dual-2560.sh
+++ scripts/disp-dtu-dual-2560.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 xrandr \
-  --output DP1 --primary --mode 2560x1440 --pos 1440x376 --rotate normal \
-  --output HDMI2 --mode 2560x1440 --pos 0x0 --rotate left \
+  --output VIRTUAL1 --off \
+  --output DP1 --mode 2560x1440 --pos 0x0 --rotate left \
+  --output HDMI2 --primary --mode 2560x1440 --pos 1440x312 --rotate normal \
   --output HDMI1 --off \
   --output DP2 --off \
   --output eDP1 --off
