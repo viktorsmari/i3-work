ed5f49cfc0dd5bafed306a326e6358ffc96b6b00
Viktor Smari <viktorsmari@gmail.com>
2017-03-24 18:36:12 +0100

    Clean scripts

--- scripts/disp-home-dualHD.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-xrandr  \
-  --output HDMI1 --mode 1920x1080 --pos 0x0 --rotate normal  \
-  --output LVDS1 --off  \
-  --output VIRTUAL1 --off  \
-  --output DP1 --off  \
-  --output VGA1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal
