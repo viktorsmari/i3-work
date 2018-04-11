b9432f54f7102ec35623087d8cb8aa688838b0fb
Viktor Smari <viktorsmari@gmail.com>
2018-04-11 16:16:30 +0200

    Git config was not saving globally

--- bootstrap.sh
+++ bootstrap.sh
@@ -97,11 +97,11 @@ if [[ $USE_GIT = 'y' ]]; then
   echo "================== Setup git ====================="
   git config --global core.editor "vim"
   git config --global push.default matching
-  git config --global user.name $MY_USER
-  git config --global user.email $MY_EMAIL
   git config merge.tool vimdiff
   git config merge.conflictstyle diff3
   git config mergetool.prompt false
+  git config --global user.name $MY_USER
+  git config --global user.email $MY_EMAIL
 
   #TODO: Generate ssh key?
   # ssh-keygen -t rsa -b 4096
