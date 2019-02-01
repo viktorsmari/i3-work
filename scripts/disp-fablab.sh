3c460bfb8b7e59f6a5059d646832bc5e7fa4f232
Viktor <viktorsmari@gmail.com>
2019-02-01 10:11:54 +0100

    Update xrandr script

--- scripts/disp-fablab.sh
+++ scripts/disp-fablab.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output VIRTUAL1 --off --output DP3 --off --output DP2 --off --output DP1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI3 --off --output HDMI2 --off --output HDMI1 --off --output LVDS1 --off --output VGA1 --mode 1920x1080 --pos 0x0 --rotate normal
+xrandr --output VGA-1 --mode 1920x1080 --pos 0x0 --rotate normal --output LVDS-1 --off --output HDMI-3 --off --output HDMI-2 --off --output HDMI-1 --off --output DP-3 --off --output DP-2 --off --output DP-1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal
