517ba49112072f3b2feaa65738f541bb7f716775
Viktor Smari <viktorsmari@gmail.com>
2019-03-15 12:21:54 +0100

    kb shortcuts for audio controls

--- generatei3.sh
+++ generatei3.sh
@@ -2,8 +2,11 @@
 HOST=$(hostname)
 
 if [ "$HOST" = leno ]; then
-    printf " using HOME leno i3 config..."
+    printf " using lenovo laptop i3 config..."
     cat ~/.i3/config.base ~/.i3/config-lenovo-home > ~/.i3/config
+elif [ "$HOST" = vtower ]; then
+    printf " using tower i3 config"
+    cat ~/.i3/config.base ~/.i3/config-tower > ~/.i3/config
 else
     printf " using WORK i3 config..."
     cat ~/.i3/config.base ~/.i3/config-work > ~/.i3/config
