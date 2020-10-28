31f962a425f6d884d100c28059c21a3c029417c9
Viktor Smari <viktorsmari@gmail.com>
2020-10-28 22:25:14 +0100

    Update example comment

--- scripts/ideaTouchpad.sh
+++ scripts/ideaTouchpad.sh
@@ -7,7 +7,7 @@ echo $MOUSEID
 DEVICE=$(xinput list-props $MOUSEID |grep 'Tapping Enabled (' | cut -c 28-30)
 echo $DEVICE
 
-# EXAMPLE: Evdev Scrolling Distance (378):	1, 1, 1
+# EXAMPLE: libinput Tapping Enabled (319):	1
 
 echo `xinput set-prop $MOUSEID $DEVICE 1`
 #reversing 1 to -1
