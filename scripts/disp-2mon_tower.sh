366457f0a3070b528462e3b9a1460cbb3e16b456
Viktor Smari <viktorsmari@gmail.com>
2021-12-10 21:46:06 +0100

    New screen 4k

--- scripts/disp-2mon_tower.sh
+++ scripts/disp-2mon_tower.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DP-1 --off --output HDMI-3 --off --output HDMI-2 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI-1 --mode 1920x1080 --pos 0x0 --rotate normal
+xrandr --output HDMI-1 --primary --mode 2560x1440 --pos 1920x0 --rotate normal --output HDMI-2 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-3 --off
