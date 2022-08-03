becc8fee78250860b50bfa1e652a3dbc1a70a397
Viktor Smari <viktorsmari@gmail.com>
2022-08-03 10:54:23 +0200

    i3: cleanup old configs

--- generatei3.sh
+++ generatei3.sh
@@ -2,15 +2,12 @@
 HOST=$(hostname)
 
 if [ "$HOST" = leno ]; then
-    printf " using lenovo laptop i3 config..."
-    cat ~/.i3/config.base ~/.i3/config-lenovo-home > ~/.i3/config
-elif [ "$HOST" = vtower ]; then
-    printf " using tower i3 config"
-    cat ~/.i3/config.base ~/.i3/config-tower > ~/.i3/config
-elif [ "$HOST" = idea ]; then
-    printf " using ideapad i3 config"
+    printf " using config-idea (Lenovo ideapad)"
     cat ~/.i3/config.base ~/.i3/config-idea > ~/.i3/config
+elif [ "$HOST" = vtower ]; then
+    printf " using config-tower i3"
+    cat ~/.i3/config.base ~/.i3/config-tower > ~/.i3/config
 else
-    printf " using WORK i3 config..."
+    printf " using config-WORK i3..."
     cat ~/.i3/config.base ~/.i3/config-work > ~/.i3/config
 fi
