96259fdab09bb7cd66de58c3614d894030127ec1
Viktor Smari <viktorsmari@gmail.com>
2023-06-15 12:36:17 +0200

    Add nm-tray

--- setup.sh
+++ setup.sh
@@ -46,7 +46,7 @@ if [[ $USE_DEP = 'y' ]]; then
   # VIP packages
   sudo apt-get install -y curl nmap zsh git g++ automake make \
     chromium-browser tree gnome-screenshot htop whois thunar bmon \
-    vim gnome-terminal xfce4-terminal
+    vim gnome-terminal xfce4-terminal nm-tray
   # Packages likely to fail:
   sudo apt-get install -y scrot silversearcher-ag pwgen gpicview powerline \
     gromit-mpx inotify-tools flameshot rofi arandr xbacklight xclip jq xfce4-clipman python3-venv ncdu \
